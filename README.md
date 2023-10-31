dea76bc0957ee1b290b2b314964c8150301140f7
Gabriel Fairbairn <97549201+NerdSlayer0@users.noreply.github.com>
2023-10-31 09:47:06 -0700

    Co-authored-by: A1exander-liU <A1exander-liU@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # comp4537_ai_hackathon
+
+AI node module found at https://www.npmjs.com/package/natural
+
+1. npm install natural
+2. const natural = require("natural"); at the top of your server.js
+3. Use the natural.Analyzer to compare two strings, seen by:
+
+   - const analyzer = new Analyzer("English", stemmer, "afinn");
+   - const result = analyzer.getSentiment(req.query.text.split(" "));
+
+4. "node server.js" in console to start
+5. localhost:8080/ai?text="the text you want to analyze"
+6. DONE!
---
+++ server.js
@@ -0,0 +1,38 @@
+const natural = require("natural");
+const express = require("express");
+const app = express();
+
+const Analyzer = natural.SentimentAnalyzer;
+const stemmer = natural.PorterStemmer;
+const analyzer = new Analyzer("English", stemmer, "afinn");
+
+const tests = [
+  { input: "I am fine" },
+  { input: "I hate this tutorial" },
+  { input: "This is an average tutorial" },
+  { input: "This is the best tutorial ever" },
+  { input: "This is the worst tutorial ever" },
+];
+
+function interpretSentiment(score) {
+  if (score > 0.5) return "Strongly Positive";
+  if (score > 0) return "Positive";
+  if (score === 0) return "Neutral";
+  if (score > -0.5) return "Negative";
+  return "Strongly Negative";
+}
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+app.get("/ai", (req, res) => {
+  if (!req.query.text) {
+    res.send("Please enter something");
+  }
+  const result = analyzer.getSentiment(req.query.text.split(" "));
+  const humanReadable = interpretSentiment(result);
+  res.send({ rating: humanReadable, score: result });
+});
+
+app.listen(8080, () => console.log("Listening"));
